3c42100dfa3b324eeea66341284ebfd78bd10a69
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2023-02-07 15:14:25 -0800

    TODO: Fix memoryManager causing index crash

---
+++ main.py
@@ -0,0 +1,96 @@
+import network
+import socket
+from time import sleep
+#from picozero import pico_temp_sensor, pico_led
+import machine
+from secrets import secrets
+import gc
+
+ssid = secrets['ssid']
+password = secrets['pw']
+
+MAC = b"\x38\xd5\x47\xaf\x99\xd7" #38:d5:47:af:99:d7
+BROADCAST_ADDR = '10.0.0.255'
+PORT = 9
+MSG = b'\xFF' * 6 + MAC * 16
+
+#Web Stuff
+def connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    ip = wlan.ifconfig()[0]
+    print('WLAN ip: \t   ',ip)
+    return ip
+
+def open_socket(ip):
+    #ip / port
+    address = ('0.0.0.0', 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    print('Socket connection: ', connection)
+    return connection
+
+def webpage(temp, state):
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+        </head>
+    <body>
+    <form action="./login">
+        <input type="text" placeholder="Enter Pass">
+        <input type="submit" value="submit">
+    </form>
+    <form action="./serverOn">
+    <input type="submit" value="Server On" />
+    </form>
+    <p>Server is {state}</p>
+    <p>Temperature is {temp}</p>
+    </body>
+    </html>
+    """
+    return str(html)
+
+def serve(connection):
+    state = 'OFF'
+    temp = 0
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        #print(request)
+        try: 
+            request = request.split()[1]
+        except IndexError:
+            pass
+        
+        if request == '/serverOn?':
+            print("Turn Server on!")
+            
+            soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            print(soc)
+            soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+            soc.sendto(MSG, (BROADCAST_ADDR, PORT))
+            
+            state = 'ON'
+        elif request == '/login?':
+            print('Login!')
+            state = 'OFF'
+        html = webpage(temp, state)
+        client.send(html)
+        client.close()
+
+try:
+    gc.enable()
+    print("Allocated: ", gc.mem_alloc(), "\nFree: ", gc.mem_free())
+    ip = connect()
+    connection = open_socket(ip)
+    serve(connection)
+except KeyboardInterrupt:
+    machine.reset()
